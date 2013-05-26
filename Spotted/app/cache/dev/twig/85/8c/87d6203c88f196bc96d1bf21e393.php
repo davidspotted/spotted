4587e2da464e51ca93e584fafb41b8d34398f657
alexis-valois <alexis@ugotspotted.com>
2013-05-26 16:32:21 -0400

    'Upload'

---
+++ Spotted/app/cache/dev/twig/85/8c/87d6203c88f196bc96d1bf21e393.php
@@ -0,0 +1,61 @@
+<?php
+
+/* FOSUserBundle:Registration:email.txt.twig */
+class __TwigTemplate_858c87d6203c88f196bc96d1bf21e393 extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+            'subject' => array($this, 'block_subject'),
+            'body_text' => array($this, 'block_body_text'),
+            'body_html' => array($this, 'block_body_html'),
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 2
+        $this->displayBlock('subject', $context, $blocks);
+        // line 7
+        $this->displayBlock('body_text', $context, $blocks);
+        // line 12
+        $this->displayBlock('body_html', $context, $blocks);
+    }
+
+    // line 2
+    public function block_subject($context, array $blocks = array())
+    {
+        // line 4
+        echo $this->env->getExtension('translator')->trans("registration.email.subject", array("%username%" => $this->getAttribute($this->getContext($context, "user"), "username"), "%confirmationUrl%" => $this->getContext($context, "confirmationUrl")), "FOSUserBundle");
+        echo "
+";
+    }
+
+    // line 7
+    public function block_body_text($context, array $blocks = array())
+    {
+        // line 9
+        echo $this->env->getExtension('translator')->trans("registration.email.message", array("%username%" => $this->getAttribute($this->getContext($context, "user"), "username"), "%confirmationUrl%" => $this->getContext($context, "confirmationUrl")), "FOSUserBundle");
+        echo "
+";
+    }
+
+    // line 12
+    public function block_body_html($context, array $blocks = array())
+    {
+    }
+
+    public function getTemplateName()
+    {
+        return "FOSUserBundle:Registration:email.txt.twig";
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  49 => 19,  23 => 4,  301 => 100,  295 => 96,  292 => 95,  289 => 94,  287 => 93,  282 => 90,  276 => 86,  273 => 85,  270 => 84,  268 => 83,  263 => 80,  249 => 79,  245 => 77,  230 => 75,  222 => 73,  220 => 72,  215 => 70,  211 => 69,  204 => 66,  198 => 63,  185 => 61,  183 => 60,  177 => 58,  160 => 57,  112 => 42,  82 => 20,  65 => 16,  38 => 8,  144 => 50,  141 => 51,  135 => 47,  126 => 45,  109 => 41,  103 => 34,  67 => 19,  61 => 16,  47 => 9,  105 => 24,  93 => 20,  76 => 22,  72 => 14,  68 => 12,  27 => 4,  91 => 20,  84 => 28,  94 => 39,  88 => 27,  79 => 19,  59 => 22,  21 => 2,  44 => 12,  31 => 6,  28 => 5,  225 => 96,  216 => 90,  212 => 88,  205 => 84,  201 => 83,  196 => 80,  194 => 62,  191 => 78,  189 => 77,  186 => 76,  180 => 72,  172 => 67,  159 => 61,  154 => 59,  147 => 55,  132 => 48,  127 => 49,  121 => 45,  118 => 44,  114 => 42,  104 => 36,  100 => 33,  78 => 24,  75 => 23,  71 => 15,  58 => 12,  34 => 11,  26 => 12,  24 => 7,  25 => 3,  19 => 2,  70 => 20,  63 => 8,  46 => 9,  22 => 2,  163 => 63,  155 => 58,  152 => 49,  149 => 51,  145 => 46,  139 => 50,  131 => 42,  123 => 47,  120 => 46,  115 => 39,  106 => 35,  101 => 36,  96 => 21,  83 => 18,  80 => 24,  74 => 16,  66 => 9,  55 => 15,  52 => 15,  50 => 10,  43 => 15,  41 => 9,  37 => 11,  35 => 8,  32 => 4,  29 => 5,  184 => 70,  178 => 71,  171 => 62,  165 => 58,  162 => 57,  157 => 56,  153 => 54,  151 => 52,  143 => 54,  138 => 51,  136 => 50,  133 => 43,  130 => 47,  122 => 44,  119 => 36,  116 => 35,  111 => 38,  108 => 37,  102 => 30,  98 => 22,  95 => 34,  92 => 28,  89 => 19,  85 => 24,  81 => 40,  73 => 19,  64 => 15,  60 => 13,  57 => 6,  54 => 10,  51 => 22,  48 => 12,  45 => 17,  42 => 9,  39 => 7,  36 => 7,  33 => 4,  30 => 2,);
+    }
+}

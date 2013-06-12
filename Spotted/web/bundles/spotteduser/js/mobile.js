645dcd904cbe347af7a6c1e715e7756da2ee5fa7
alexis-valois <alexis@ugotspotted.com>
2013-06-12 07:56:50 -0400

    deliver

---
+++ Spotted/web/bundles/spotteduser/js/mobile.js
@@ -0,0 +1,40 @@
+function connect(){
+	$("#login-form").submit();
+}
+
+function register(){
+	$("#register-form").submit();
+}
+
+function pageSetup(){
+	var containerWidth = $("#mobileContainer").width();
+	var captionWidth = (containerWidth - 9)/2;
+	$(".div-home-caption").css("width", captionWidth);
+	
+	$("#div-signup-form").hide();
+}
+
+function formToogle(sender){
+	
+	if (sender.id == "div-login-caption"){
+		//alert("LOGIN");
+		if (!$("#div-login-form").is(":visible")){
+			$("#div-login-form").show("slide");
+
+		}
+		$("#div-signup-form").hide("slide");
+
+	}
+	
+	if (sender.id == "div-register-caption"){
+		//alert("REGISTER");
+		if (!$("#div-signup-form").is(":visible")){
+			$("#div-signup-form").show("slide");	
+
+		}
+		$("#div-login-form").hide("slide");
+
+	}
+	
+	
+}
